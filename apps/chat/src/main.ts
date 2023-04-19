58c2e4266b77cee97d8a82cb2f1c965f7b0cbc80
Bryan Hannes <bryan@zoli.be>
2023-04-19 18:49:50 +0200

    feat: update tab title based on active chat

--- apps/chat/src/main.ts
+++ apps/chat/src/main.ts
@@ -1,15 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
   provideRouter,
+  TitleStrategy,
   withEnabledBlockingInitialNavigation
 } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { ChatTitleStrategy } from '../../../libs/chat/util-config/src/lib/chat-title-strategy.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    provideHttpClient()
+    provideHttpClient(),
+    {
+      provide: TitleStrategy,
+      useClass: ChatTitleStrategy
+    }
   ]
 }).catch((err) => console.error(err));
---
+++ libs/chat/util-config/README.md
@@ -0,0 +1,7 @@
+# chat-util-config
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test chat-util-config` to execute the unit tests.
